adeb03b58e0091c02573b94f3ffd0f65807ca16b
Gargo83 <psollberger83@gmail.com>
2013-01-25 22:35:32 +0100

    New Settings for Position update Interval in active and passive Mode

--- app/src/ch/n3utrino/enlatitude/EnlAtitudePreferences.java
+++ app/src/ch/n3utrino/enlatitude/EnlAtitudePreferences.java
@@ -9,9 +9,15 @@ import java.util.UUID;
 
 public class EnlAtitudePreferences {
 
-    public static final String PREF_ID = "PREF_ID";
     public static final String EMPTY = "EMPTY";
 
+    public static final String KEY_PREF_ID = "PREF_ID";
+    public static final String KEY_PREF_USERNAME = "username";
+    public static final String KEY_PREF_UPDATE_SPEED_FOREGROUND = "updateSpeedForeground";
+    public static final String KEY_PREF_UPDATE_SPEED_BACKGROUND = "updateSpeedBackground";
+
+    public static final int    DEFAULT_UPDATE_SPEED_FOREGROUND = 20;    //Also adjust in preferences.xml
+    public static final int    DEFAULT_UPDATE_SPEED_BACKGROUND = 300;   //Also adjust in preferences.xml
 
     private SharedPreferences mPreferences;
 
@@ -21,11 +27,11 @@ public class EnlAtitudePreferences {
 
 
     public String getUuid(){
-        String uuid = mPreferences.getString(PREF_ID, EMPTY);
+        String uuid = mPreferences.getString(KEY_PREF_ID, EMPTY);
 
         if(uuid.equals(EMPTY)){
             uuid = UUID.randomUUID().toString();
-            mPreferences.edit().putString(PREF_ID,uuid).commit();
+            mPreferences.edit().putString(KEY_PREF_ID,uuid).commit();
 
         }
 
@@ -38,15 +44,39 @@ public class EnlAtitudePreferences {
         data.setUuid(getUuid());
 
         return data;
-
-
     }
 
     public void setUsername(String newUsername){
-        mPreferences.edit().putString("username",newUsername).commit();
+        mPreferences.edit().putString(KEY_PREF_USERNAME,newUsername).commit();
     }
 
     public String getUserName(){
-        return mPreferences.getString("username","");
+        return mPreferences.getString(KEY_PREF_USERNAME,"");
+    }
+
+    public void setUpdateSpeedForeground(int seconds) {
+        mPreferences.edit().putInt(KEY_PREF_UPDATE_SPEED_FOREGROUND,seconds).commit();
+    }
+
+    public int getUpdateSpeedForeground() {
+        try {
+            return Integer.parseInt(mPreferences.getString(KEY_PREF_UPDATE_SPEED_FOREGROUND,Integer.toString(DEFAULT_UPDATE_SPEED_FOREGROUND)));
+        }
+        catch (Exception ex) {
+            return DEFAULT_UPDATE_SPEED_FOREGROUND;
+        }
+    }
+
+    public void setUpdateSpeedBackground(int seconds) {
+        mPreferences.edit().putInt(KEY_PREF_UPDATE_SPEED_BACKGROUND,seconds).commit();
+    }
+
+    public int getUpdateSpeedBackground() {
+        try {
+            return Integer.parseInt(mPreferences.getString(KEY_PREF_UPDATE_SPEED_BACKGROUND,Integer.toString(DEFAULT_UPDATE_SPEED_BACKGROUND)));
+        }
+        catch (Exception ex) {
+            return DEFAULT_UPDATE_SPEED_BACKGROUND;
+        }
     }
 }
